e49622e7b11855e5b58efae5fd4ac16dcd81c178
gaurav-opensource <gauravopensource95@gmail.com>
2024-10-13 00:17:15 +0530

    Initial commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,6 @@
 const Footer = () => {
 	return (
-	  <footer className="py-8 bg-black text-gray-400 border-t border-gray-800">
-		
-		  <p className="text-center mt-4 text-xs">
-			© 2024 Netflix, Inc.
-		  </p>
-		
-	  </footer>
+	  <h1></h1>
 	);
   };
   
